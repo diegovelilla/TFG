b1f0002a47475c667e159bca09fcedf65d5f1843
diego.velilla <diegovelillarecio@gmail.com>
2025-06-22 01:08:32 +0200

    init

--- CraftingTable/__init__.py
+++ CraftingTable/__init__.py
@@ -1,6 +1,6 @@
 from .models.ctgan import CTGAN
 from .models.tvae import TVAE
-from .models.tabddpm_mlp import TabDDPM
+from .models.tabddpm import TabDDPM
 from .models.tabsyn import TabSyn
 
 __all__ = [
